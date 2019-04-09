100b999d1d5cdefb2575c7d07eef34e5e20188ec
sebug <sebastian.gfeller@gmail.com>
2019-04-09 10:43:49 +0200

    Filtering out people without phone number

--- create_vcards.py
+++ create_vcards.py
@@ -1,8 +1,22 @@
 import csv
+
+class Astreint:
+    """Somebody conscripted to civil protection"""
+    def __init__(self, row):
+        self.name = row[0]
+        self.first_name = row[1]
+        self.function = row[2]
+        self.phone = row[3]
+
+
 with open('astreints.csv', newline='') as csvfile:
     astreintsreader = csv.reader(csvfile, delimiter=';')
-    for row in astreintsreader:
-        print(', '.join(row))
+    astreintsreader.__next__() # skip header
+    astreints = map(Astreint, astreintsreader)
+    astreints_with_phone = filter(lambda a: a.phone, astreints)
+    for astreint in astreints_with_phone:
+        print(astreint.phone)
+        
 
 
 
